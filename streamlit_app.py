b7f0c4d3c2982a608dd433422835295ff6cb4b3b
Jan van Zetten <40922303+janrott@users.noreply.github.com>
2023-07-22 18:49:50 +0200

    Update streamlit_app.py
    added smotthy header

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,10 @@ import streamlit
 
 streamlit.title('My Parent New Healthy Diner')
 
-streamlit.header('🥣 Breakfast menu')
-streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
+streamlit.header(' Breakfast favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Advocado toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
